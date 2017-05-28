2d381a57ca1f6ec4c8017ab02b55c7b9f7b64d91
moneunchul93 <moneunchul93@naver.com>
2017-05-28 21:04:20 +0900

    처음 생성. person 클래스 추가

---
+++ app/src/main/res/Person.java
@@ -0,0 +1,14 @@
+/**
+ * Created by moneunchul93 on 2017-05-28.
+ */
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Person {
+    private char name;
+    private int age;
+    private int year,month,date;
+
+    Date d = new Date();
+    System.out.println(d);
+}
